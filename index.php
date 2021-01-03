729615343879c21c3254daf29d078cf51e1a1ef0
Ksu-S <48758104+Ksu-S@users.noreply.github.com>
2021-01-03 10:25:57 +0200

    3

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
 
-require 'translations/translate.php';
+require 'vendor/autoload.php';
+
+use Google\Cloud\Translate\V3\TranslationServiceClient;
+
+$translationServiceClient = new TranslationServiceClient();
+
+?>
+<INPUT TYPE = "Text" VALUE ="text" NAME = "text">
+<?php
+$username = $_POST['text'];
+// $text = 'Hello, world!';
+$targetLanguage = 'fr';
+$projectId = 'AIzaSyC8WSQX5pjS41THP_YfG5fvvAY66DvOhw4';
+$contents = [$text];
+$formattedParent = $translationServiceClient->locationName($projectId, 'global');
+
+try {
+    $response = $translationServiceClient->translateText(
+        $contents,
+        $targetLanguage,
+        $formattedParent
+    );
+    foreach ($response->getTranslations() as $translation) {
+        printf('Translated text: %s' . PHP_EOL, $translation->getTranslatedText());
+    }
+} finally {
+    $translationServiceClient->close();
+}
