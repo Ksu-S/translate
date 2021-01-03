f0b9e1304e1f5e02da9956b8e7393cf00f0ddb60
Ksu-S <48758104+Ksu-S@users.noreply.github.com>
2021-01-03 15:24:07 +0200

    6

---
+++ resources/views/translate.blade.php
@@ -0,0 +1,51 @@
+<?php
+
+require '../vendor/autoload.php';
+
+
+use Google\Cloud\Translate\V3\TranslationServiceClient;
+
+$translationServiceClient = new TranslationServiceClient();
+
+?>
+
+<form action="/" method="post">
+  Write text
+  <input type="text" name="translate" maxlength="50" />
+
+  Please choose language
+  <select name="targetLanguage">
+     <option value="">Select...</option>
+     <option value="de">de</option>
+     <option value="ru">ru</option>
+  </select>
+
+<input type="submit" name="formSubmit" value="Submit" name="formSubmit" />
+</form>
+
+<?php
+
+
+if($_POST['formSubmit'] == "Submit") 
+{
+    $translate = $_POST['translate'];
+    $targetLanguage = $_POST['targetLanguage'];
+    $projectId = 'mercurial-ruler-300412';
+    $contents = [$translate];
+    $formattedParent = $translationServiceClient->locationName($projectId, 'global');
+
+try {
+    $response = $translationServiceClient->translateText(
+        $contents,
+        $targetLanguage,
+        $formattedParent
+    );
+    // Display the translation for each input text provided
+    foreach ($response->getTranslations() as $translation) {
+        printf('Translated text: %s' . PHP_EOL, $translation->getTranslatedText());
+    }
+} finally {
+    $translationServiceClient->close();
+}
+
+}
